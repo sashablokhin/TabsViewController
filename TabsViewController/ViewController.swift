ee72e8440825add1bf1ee1855ff6e18111a228eb
sashablokhin <sashablokhindev@gmail.com>
2016-02-02 15:27:02 +0300

    Configure navigation bar

--- TabsViewController/ViewController.swift
+++ TabsViewController/ViewController.swift
@@ -12,7 +12,27 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let titleLabel = UILabel()
+        titleLabel.text = "Главная"
+        titleLabel.textColor = UIColor.whiteColor()
+        //titleLabel.font = UIFont(name: "System", size: 17)!
+        titleLabel.sizeToFit()
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: titleLabel)
+        
+        let searchButton: UIButton = UIButton(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        searchButton.setImage(UIImage(named: "search.png"), forState: UIControlState.Normal)
+        //searchButton.addTarget(self, action: "searchButtonPressed", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        searchButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 10)
+        
+        let menuButton: UIButton = UIButton(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        menuButton.setImage(UIImage(named: "menu.png"), forState: UIControlState.Normal)
+        //menuButton.addTarget(self, action: "menuButtonPressed", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        menuButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: -10)
+        
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: menuButton), UIBarButtonItem(customView: searchButton)]
     }
 
     override func didReceiveMemoryWarning() {
